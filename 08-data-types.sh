193247fcb368bee74574f54f79f6490b5ad56f1b
Venugopal Reddy-M <venuaws.mvgr27@gmail.com>
2026-02-18 16:54:17 +0530

    passing all args in array

--- 08-data-types.sh
+++ 08-data-types.sh
@@ -6,3 +6,13 @@ NUM2=21
 sum=$(($NUM1+$NUM2))
 
 echo "total sum is: $sum"
+
+#Array decleractions
+ FRUITS=("apple" "banana" "mango")
+
+ # $@ passed all args 
+
+echo "Fruits are: ${FRUITS[@]}"
+echo "First Fruit is: ${FRUITS[0]}"
+echo "Second Fruit is: ${FRUITS[1]}"
+echo "Third Fruit is: ${FRUITS[2]}"
