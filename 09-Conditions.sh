764b9281f2525dd001e123e525e6498943c8f5a1
Venugopal Reddy-M <venuaws.mvgr27@gmail.com>
2026-02-20 11:12:17 +0530

    condiction test

---
+++ 09-Conditions.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+NUMBER=$1
+
+if[$NUMBER -gt 20]; then
+  echo "Given number :$NUMBER is grater then 20 "
+elif[$NUMBER -el 20]; then
+  echo "Given number :$NUMBER is eqal 20
+else 
+   echo "Given number:$NUMBER is less then 20"
+if
