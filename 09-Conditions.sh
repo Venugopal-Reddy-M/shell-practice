70c1ff910c257e0fd8eb44bfbe9cde60d8129d48
Venugopal Reddy-M <venuaws.mvgr27@gmail.com>
2026-02-20 11:21:29 +0530

    condition added

--- 09-Conditions.sh
+++ 09-Conditions.sh
@@ -2,10 +2,10 @@
 
 NUMBER=$1
 
-if [$NUMBER -gt 20]; then
-  echo "Given number :$NUMBER is grater then 20 "
-elif [$NUMBER -el 20]; then
-  echo "Given number :$NUMBER is eqal 20
+if [ $NUMBER -gt 20 ]; then
+  echo "Given number: $NUMBER is grater then 20"
+elif [ $NUMBER -eq 20 ]; then
+  echo "Given number: $NUMBER is eqal 20"
 else 
    echo "Given number:$NUMBER is less then 20"
 if
