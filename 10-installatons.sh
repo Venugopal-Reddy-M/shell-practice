39a08b26eceacb8816fb09e20fe288beb6858019
Venugopal Reddy-M <venuaws.mvgr27@gmail.com>
2026-02-20 12:52:43 +0530

    installing nginx

--- 10-installatons.sh
+++ 10-installatons.sh
@@ -5,6 +5,18 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]; then
   echo "please run the srcipt root user access"
   exit 1
+else
+   echo "runnig in root user only"
 fi
 
 echo "Installing Nginx"
+
+# dnf install nginx -y
+
+#if [ $? -ne 0 ]; then
+#   echo "Install nginx ....FAILURE"
+#   exit 1
+#else
+#   echo "Istalling ngin ....Success"
+#fi
+    
