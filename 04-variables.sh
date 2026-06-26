13e8b01a055810d2c6ac49503ef169265d394bab
Venugopal Reddy-M <venuaws.mvgr27@gmail.com>
2026-06-26 21:27:17 +0530

    args passed to dynamically

--- 04-variables.sh
+++ 04-variables.sh
@@ -6,4 +6,4 @@ PERSON3=$3
 # arrguments passed to the script ex: 04-variables.sh venu ramu 
 echo "$PERSON1:: hi $2 how are you now"
 echo "$PERSON2:: hello $1, im fine"
-ech0 "$PERSON3:: nice to meet you $1 and $2."
+echo "$PERSON3:: nice to meet you $1 and $2"
