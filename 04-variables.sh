61aab0fd90993e09bb79028a9d51d478a753c37e
Venugopal Reddy-M <venuaws.mvgr27@gmail.com>
2026-06-26 21:22:54 +0530

    args passed to dynamocally

--- 04-variables.sh
+++ 04-variables.sh
@@ -2,6 +2,8 @@
 
 PERSON1=$1
 PERSON2=$2
+PERSON3=$3
 # arrguments passed to the script ex: 04-variables.sh venu ramu 
 echo "$PERSON1:: hi $2 how are you now"
 echo "$PERSON2:: hello $1, im fine"
+ech0 "$PERSON3:: nice to meet you $1 and $2."
